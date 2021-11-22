e48823b32474bf42494104bdd38cb402464d336d
Stefano Franco <stefano.franco@nuvibit.com>
2021-11-22 09:23:14 +0100

    fix: reverting chaos

--- modules/execution-role/main.tf
+++ modules/execution-role/main.tf
@@ -19,33 +19,25 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# ¦ LOCALS
-# ---------------------------------------------------------------------------------------------------------------------
-locals {
-  execution_role_name_concat = format(
-    "%s_execution_role%s",
-    var.function_name,
-  )
-
-  execution_role_name = var.create_execution_role ? (var.iam_execution_role_name == null ? local.execution_role_name_concat : var.iam_execution_role_name) : replace(split(":", var.iam_execution_role_arn)[5], "role/", "")
+data "aws_iam_role" "external_execution" {
+  count = var.create_execution_role ? 0 : 1
+  name  = var.iam_execution_role_external_name
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ¦ IAM LAMBDA EXECUTION ROLE
+# ¦ IAM LAMBDA ROLE
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role" "lambda" {
   count = var.create_execution_role ? 1 : 0
 
-  name                 = local.execution_role_name
+  name                 = var.iam_execution_role_name
   assume_role_policy   = data.aws_iam_policy_document.lambda.json
   permissions_boundary = var.iam_execution_role_permissions_boundary_arn
   tags                 = var.resource_tags
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ¦ IAM LAMBDA EXECUTION ROLE - TRUST POLICY
+# ¦ IAM EXECUTION POLICY
 # ---------------------------------------------------------------------------------------------------------------------
 data "aws_iam_policy_document" "lambda" {
   statement {
@@ -61,18 +53,18 @@ data "aws_iam_policy_document" "lambda" {
   }
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# ¦ IAM LAMBDA EXECUTION ROLE - POLICIES
-# ---------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role_policy_attachment" "lambda" {
-  count = length(var.iam_execution_policy_arns)
+  count = var.create_execution_role ? length(var.iam_execution_policy_arns) : 0
 
-  role       = local.execution_role_name
+  role       = aws_iam_role.lambda[0].name
   policy_arn = var.iam_execution_policy_arns[count.index]
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ IAM LOGGING POLICY
+# ---------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role_policy" "lambda_logs" {
-  role   = local.execution_role_name
+  role   = var.create_execution_role ? aws_iam_role.lambda[0].name : data.aws_iam_role.external_execution[0].name
   policy = data.aws_iam_policy_document.lambda_logs.json
 }
 
