d7bf2a15bdfc953e0d1fcc296f6f294cac946e65
nuvibit-michael <michael.ullrich@nuvibit.com>
2022-02-14 18:22:37 +0100

    change order

--- variables.tf
+++ variables.tf
@@ -179,7 +179,7 @@ variable "triggering_sns_topics" {
 
   validation {
     condition = var.triggering_sns_topics == [] ? true : alltrue([
-      for p in var.triggering_sns_topics : (can(regex("^arn:aws:sns:", p.sns_arn)) || p.sns_arn == null)
+      for p in var.triggering_sns_topics : (p.sns_arn == null || can(regex("^arn:aws:sns:", p.sns_arn)))
     ])
     error_message = "Values must contain SNS ARN, starting with \"arn:aws:sns:\" and an optional filter_policy_json."
   }
