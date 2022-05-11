e0f9e1e496b4087480a4d12e603ef93b0c2ab436
Stefano Franco <87094783+stefano-franco@users.noreply.github.com>
2022-05-11 17:15:43 +0200

    chore: limit egress policy in example

--- examples/lambda-vpc/main.tf
+++ examples/lambda-vpc/main.tf
@@ -173,7 +173,6 @@ resource "aws_security_group" "allow_https" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["1.2.3.4/32]
   }
 }
