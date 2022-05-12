6a39f3744d8bf8dbf4276d31fb6fbb5526961d64
Jonas Saegesser <jonas.saegesser@nuvibit.com>
2022-05-12 21:02:14 +0200

    set enable_encryption = true in sns example

--- examples/lambda-sns/main.tf
+++ examples/lambda-sns/main.tf
@@ -206,6 +206,7 @@ module "lambda" {
   timeout              = 360
   runtime              = "python3.9"
   kms_key_arn          = aws_kms_key.example.arn
+  enable_encryption    = true
   resource_tags        = var.resource_tags
   resource_name_suffix = random_string.suffix.result
   tracing_mode         = "Active"
