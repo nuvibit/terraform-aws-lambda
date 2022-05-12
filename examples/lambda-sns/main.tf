f591f5f5ff358149b6763fb3c6be9e449b463056
Jonas Saegesser <jonas.saegesser@nuvibit.com>
2022-05-12 08:25:46 +0200

    fix: enable key rotation in sns example

--- examples/lambda-sns/main.tf
+++ examples/lambda-sns/main.tf
@@ -78,6 +78,7 @@ resource "random_string" "suffix" {
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_kms_key" "example" {
   description = format("%s-key", var.function_name)
+  enable_key_rotation = true
 }
 
 data "aws_iam_policy_document" "key_policy" {
