75b0e878f15df96438c50a03e9323000f7d630a3
michael-ullrich-1010 <michael.ullrich@nuvibit.com>
2022-09-12 12:44:57 +0200

    add test-case

---
+++ examples/lambda-explicit-trust/main.tf
@@ -0,0 +1,123 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ PROVIDER
+# ---------------------------------------------------------------------------------------------------------------------
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ BACKEND
+# ---------------------------------------------------------------------------------------------------------------------
+terraform {
+  backend "remote" {
+    organization = "nuvibit"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "aws-s-testing"
+    }
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ VERSIONS
+# ---------------------------------------------------------------------------------------------------------------------
+terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 3.15"
+      configuration_aliases = []
+    }
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ DATA
+# ---------------------------------------------------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LOCALS
+# ---------------------------------------------------------------------------------------------------------------------
+locals {
+  execution_policy_name = format(
+    "%s_execution_policy-%s",
+    var.function_name,
+    random_string.suffix.result,
+  )
+  event_patterns = [
+    jsonencode(
+      {
+        "source" : ["aws.ec2"],
+        "detail-type" : ["EC2 Instance State-change Notification"],
+        "detail" : {
+          "state" : ["terminated"]
+        }
+      }
+    )
+  ]
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ RANDOM SUFFIX
+# ---------------------------------------------------------------------------------------------------------------------
+resource "random_string" "suffix" {
+  length  = 16
+  number  = true
+  lower   = true
+  upper   = true
+  special = false
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LAMBDA EXECUTION POLICIES
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_iam_policy" "list_users" {
+  name   = local.execution_policy_name
+  policy = data.aws_iam_policy_document.list_users.json
+}
+
+data "aws_iam_policy_document" "list_users" {
+  # enable IAM in logging account
+  statement {
+    sid       = "EnableOrganization"
+    effect    = "Allow"
+    actions   = ["iam:ListUsers"]
+    resources = ["*"]
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LAMBDA
+# ---------------------------------------------------------------------------------------------------------------------
+#tfsec:ignore:aws-lambda-enable-tracing
+module "lambda" {
+  # source  = "nuvibit/lambda/aws"
+  # version = "~> 1.0"
+  source = "../../"
+
+  function_name           = var.function_name
+  description             = var.description
+  package_source_path     = "${path.module}/lambda_files"
+  handler                 = "main.lambda_handler"
+  schedule_expression     = "cron(0 12 * * ? *)"
+  event_patterns          = local.event_patterns
+  iam_execution_role_path = "/lambda/"
+  iam_execution_policy_arns = [
+    aws_iam_policy.list_users.arn
+  ]
+  iam_execution_role_explicit_trust = true
+  environment_variables = {
+    ACCOUNT_ID = data.aws_caller_identity.current.account_id
+  }
+  memory_size          = 128
+  timeout              = 360
+  runtime              = "python3.9"
+  resource_tags        = var.resource_tags
+  resource_name_suffix = random_string.suffix.result
+  tracing_mode         = "PassThrough"
+}
