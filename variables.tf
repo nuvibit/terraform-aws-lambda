87cbfe6b4c5e038c0c982a44b720e468a35638b2
michael-ullrich-1010 <michael.ullrich@nuvibit.com>
2022-09-12 08:35:12 +0200

    update runtimes

--- variables.tf
+++ variables.tf
@@ -82,21 +82,19 @@ variable "timeout" {
   }
 }
 
+# https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
 variable "runtime" {
   description = "Identifier of the function's runtime. See Runtimes for valid values."
   type        = string
 
   validation {
     condition = contains([
-      "nodejs", "nodejs4.3", "nodejs6.10",
-      "nodejs8.10", "nodejs10.x", "nodejs12.x",
-      "nodejs14.x", "nodejs16.x", "java8", "java8.al2",
-      "java11", "python2.7", "python3.6",
+      "nodejs12.x", "nodejs14.x", "nodejs16.x", 
+      "java8", "java8.al2", "java11",      
       "python3.7", "python3.8", "python3.9",
-      "dotnetcore1.0", "dotnetcore2.0", "dotnetcore2.1",
       "dotnetcore3.1", "dotnet6",
-      "nodejs4.3-edge", "go1.x",
-      "ruby2.5", "ruby2.7", "provided", "provided.al2"
+      "go1.x", "ruby2.7", 
+      "provided", "provided.al2"
     ], var.runtime)
     error_message = "Identifier of the function's runtime must be supported by AWS Lambda."
   }
