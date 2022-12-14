e430d9bfb75de0d5497df51076524965c2ced24b
michael-ullrich-1010 <michael.ullrich@nuvibit.com>
2022-12-14 08:51:43 +0100

    fix

--- modules/execution-role/main.tf
+++ modules/execution-role/main.tf
@@ -29,7 +29,7 @@ data "aws_iam_role" "external_execution" {
 # ¦ LOCALS
 # ---------------------------------------------------------------------------------------------------------------------
 locals {
-  region_name_splitted = split("-", dat.aws_region.current.name)
+  region_name_splitted = split("-", data.aws_region.current.name)
   region_name_short = "${local.region_name_splitted[0]}${substr(local.region_name_splitted[1], 0, 1)}${local.region_name_splitted[2]}"
 }
 
